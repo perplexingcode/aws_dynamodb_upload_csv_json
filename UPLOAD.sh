d462715e60d40f5ec4302dc9cf16a43757b534d2
David Bui <perplexingcode@gmail.com>
2022-04-29 18:09:49 +0700

    log upload result into a file

--- UPLOAD.sh
+++ UPLOAD.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 avg=0
-item=0
+pack=0
 start_timet=$(date +%s)
-
+echo "--------------------" >> upload_log.txt
+echo $(date +'%Y/%m/%d') >> upload_log.txt
 search_dir="O:\OneDrive\Projects\MFVN\AWS_DB_upload_CSV_JSON\output"
 index=0
 for entry in "$search_dir"/*
 do
 #Upload
-((item++))
-printf "Item: %s\n" "$item"
+((pack++))
+printf "Package: %s\n" "$pack"
 start_time=$(date +%s)
 #AWS	
     aws dynamodb batch-write-item --request-items file://$entry 
@@ -25,10 +26,13 @@ done
 #Finish
 end_timet=$(date +%s)
 total=$(( end_timet - start_timet ))
-avg=$((total/item))
+avg=$((total/pack))
 totalm=$((total/60))
-printf "Upload complete!\n"
-printf "Average item upload time: %s seconds.\n" "$avg"
-printf "Total time: %s minutes.\n" "$totalm"
-printf "Total time: %s seconds.\n" "$total"
+items=$((pack*25))
+line="------------------------"
+printf "%s\n" "$line"
+printf "Upload completed!\n"
+printf "%s packages ~%s items uploaded\n" "$pack" "$items"
+printf "Average package upload time: %s seconds.\n" "$avg"
+printf "Total time: %s minutes (%s seconds).\n" "$totalm" "$total" >> upload_log.txt
 sleep 30m
